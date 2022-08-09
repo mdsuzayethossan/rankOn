cfd5ab725b1c6f30ec37e8bae7fb37fdaee1c9ec
Md Suzayet Hossan <md.suzayet.377@gmail.com>
2022-08-09 23:03:24 +0600

    responsive

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,18 @@
 
     document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
         document.querySelector('.offcanvas-collapse').classList.toggle('open')
-    })
+    });
+
+       //navbar
+       var navbar = $('.navbar');
+
+       $(window).scroll(function() {
+           if ($(window).scrollTop() > 200) {
+           navbar.addClass('bg-nav');
+           } else {
+           navbar.removeClass('bg-nav');
+           }
+       });
 
     //review
     $('.review-slider-main').slick({
@@ -26,5 +37,33 @@
         dots: true,
         centerMode: true,
         centerPadding: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 900,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       });
 })()
