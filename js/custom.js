1d4f906b8b3649368b07b0ffe76fe966da17b3b3
Md Suzayet Hossan <md.suzayet.377@gmail.com>
2022-08-17 10:27:00 +0600

    final

--- js/custom.js
+++ js/custom.js
@@ -4,19 +4,31 @@
     document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
         document.querySelector('.offcanvas-collapse').classList.toggle('open')
     });
+  //------------------------ back to top ------------------//
+    var navbar = $('.navbar');
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 200) {
+        navbar.addClass('bg-nav');
+        } else {
+        navbar.removeClass('bg-nav');
+        }
+    });
+    //------------------------ back to top ------------------//
+    var btn = $('.back-to-top');
 
-       //navbar
-       var navbar = $('.navbar');
+    $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+    });
+    btn.on('click', function(e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: "0" },  5000);
+    });
 
-       $(window).scroll(function() {
-           if ($(window).scrollTop() > 200) {
-           navbar.addClass('bg-nav');
-           } else {
-           navbar.removeClass('bg-nav');
-           }
-       });
-
-    //review
+    //------------------------ review ------------------//
     $('.review-slider-main').slick({
         slidesToShow: 2,
         slidesToScroll: 1,
@@ -27,7 +39,56 @@
         nextArrow: $('.review-down-icon')
 
       });
-    //news
+    //------------------------ counter ------------------//
+    $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+  });
+    //------------------------ venobox ------------------//
+     new VenoBox({
+      selector: '.my-play',
+    });
+    //------------------------ client-brand ------------------//
+    $('.client-brand').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        centerPadding: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 900,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+      //------------------------ news ------------------//
     $('.news-slider-main').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
